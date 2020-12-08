d9f1d0efc5a3c19bd4d24da2be975eb88fb82720
Cooper Barth <cooperfbarth@gmail.com>
2020-12-08 01:09:52 -0600

    Add keyboard shortcut to change focus to next label of same name

--- src/components/TrackArea.tsx
+++ src/components/TrackArea.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import "./TrackArea.css";
 
 import AudioControls from "./AudioControls";
@@ -23,6 +24,13 @@ interface TrackAreaState {
 type MouseEvent = React.MouseEvent<HTMLDivElement, MouseEvent>;
 type KeyboardEvent = React.KeyboardEvent<HTMLDivElement>;
 
+enum ArrowKey {
+    Up = "arrowup",
+    Down = "arrowdown",
+    Left = "arrowleft",
+    Right = "arrowright"
+}
+
 class TrackArea extends Component<TrackAreaProps, TrackAreaState> {
 
     playheadStepInterval?: number;
@@ -36,8 +44,57 @@ class TrackArea extends Component<TrackAreaProps, TrackAreaState> {
     constructor(props: TrackAreaProps) {
         super(props);
         this.state = this.defaultState;
+    }
 
-        window.document.onkeydown = this.onKeyDown.bind(this);
+    componentDidMount() {
+        window.addEventListener("keydown", this.onKeyDown.bind(this));
+    }
+
+    createLabel = (x: number) => {
+        const ref = React.createRef<HTMLDivElement>();
+        this.setState({ labels: [...this.state.labels, new LabelInfo(x, ref)] });
+    }
+
+    focusNextOrPrevMatchingLabel = (direction: ArrowKey.Left | ArrowKey.Right) => {
+        // I love how well-documented and intuitive setting up custom focus in React is :)
+        const labels = this.state.labels;
+        let i = 0;
+        for (const info of labels) {
+            // Find if the label has focus
+            if (window.document.activeElement === ReactDOM.findDOMNode(info.ref.current)) {
+                break;
+            }
+            i++;
+        }
+
+        // Label i has focus
+        if (i < labels.length) {
+            const increment = direction === ArrowKey.Left ? -1 : 1;
+            const boundCondition = direction === ArrowKey.Left ?
+                                   (i: number) => i >= 0 :
+                                   (i: number) => i < labels.length;
+
+            // Find the next label with the same text
+            for (let j = i + increment; boundCondition; i += increment) {
+                if (labels[i].text === labels[j].text) {
+                    // Change focus to the matching label
+                    labels[j].ref.current.focus();
+                    break;
+                }
+            }
+        }
+    }
+
+    onKeyDown = (e: KeyboardEvent) => {
+        const key = e.key.toLowerCase();
+        if (key === "l") {
+            this.createLabel(this.state.playheadPosition);
+        } else if (e.shiftKey) {
+            // Navigate to the next matching label (if one is currently focused)
+            if (key === ArrowKey.Right || key === ArrowKey.Left) {
+                this.focusNextOrPrevMatchingLabel(key);
+            }
+        }
     }
 
     calculateInterval(): number {
@@ -66,8 +123,7 @@ class TrackArea extends Component<TrackAreaProps, TrackAreaState> {
     }
 
     onLabelAreaClick = (e: MouseEvent) => {
-        const x = e.pageX - e.currentTarget.offsetLeft;
-        this.setState({ labels: [...this.state.labels, new LabelInfo(x)] });
+        this.createLabel(e.pageX - e.currentTarget.offsetLeft);
     }
 
     onPlayheadAreaClick = (e: MouseEvent) => {
@@ -112,23 +168,14 @@ class TrackArea extends Component<TrackAreaProps, TrackAreaState> {
         }
     }
 
-    onKeyDown = (e: KeyboardEvent) => {
-        if (e.key.toLowerCase() === "l") {
-            this.setState({ labels: [...this.state.labels, new LabelInfo(this.state.playheadPosition)] });
-        }
-    }
-
     render() {
         return <div className="track-area"
-                    onKeyDown={this.onKeyDown.bind(this)}
                     style={{ width: StyleConstants.TrackAreaWidth }}>
             <div className="label-area"
                  onClick={this.onLabelAreaClick.bind(this)}>
-                {this.state.labels.map((label: LabelInfo) => 
-                    <Label key={label._id}
-                           info={label}
-                           onClickHandler={this.onLabelClick.bind(this)}/>
-                )}
+                {this.state.labels.map((label: LabelInfo) => <Label key={label._id}
+                                   info={label}
+                                   onClickHandler={this.onLabelClick.bind(this)}/>)}
             </div>
             <div className="playhead-area"
                  onClick={this.onPlayheadAreaClick.bind(this)}>
